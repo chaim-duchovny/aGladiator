aa4f3c1bcc3370ef5e80ab4f465da97cfc763f01
chaim-duchovny <chaim.duchovny@gmail.com>
2025-02-21 23:58:30 +0200

    Update square.py

--- src/square.py
+++ src/square.py
@@ -1,13 +1,11 @@
 class Square:
 
-    def __init__(self, row, col, number = None, piece = None):
+    def __init__(self, row, col, number = None, name = None, piece = None):
         self.row = row
         self.col = col
         self.number = number
+        self.name = name
         self.piece = piece
 
     def has_piece(self):
         return self.piece != None
-
-
-
